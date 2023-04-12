491cde2c1834e5cef5ec15852850565a1345dfcd
SALVADORPOETA <dsml@live.com.mx>
2023-04-12 14:02:24 -0600

    env

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,14 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCr8By1bXQEoxJv59rbrK1Yew9lCSW82hs",
-  authDomain: "auth-yt-9624d.firebaseapp.com",
-  projectId: "auth-yt-9624d",
-  storageBucket: "auth-yt-9624d.appspot.com",
-  messagingSenderId: "597907559036",
-  appId: "1:597907559036:web:eed9629d298a053ad5eff9"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
